fa5fad634edc0cbd4cf55ef6cb987c615433dac5
f359412 <michael_feng_wan@freddiemac.com>
2024-06-09 13:40:37 -0400

    first commit

---
+++ src/webparts/hello4/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHello4WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'Hello4WebPartStrings' {
+  const strings: IHello4WebPartStrings;
+  export = strings;
+}
